b7868ed3e37ad8d203ab2cd0a78d36c48e29a600
anastynero <anastasia1995ggs@gmail.com>
2025-03-24 18:18:06 +0300

    Promo

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -10,27 +10,32 @@ function Catalog() {
   return(
     <div className='catalog'>
       <div className='container'>
+      <div className='catalog-wrapper'>
+        <div className='catalog-title'>
+         <h2>Репродукции</h2>
+          </div>
       <div className="countrySelector">
         <button className="country-btn" onClick={() => setSelectedCountry('france')}>Франция</button>
         <button className="country-btn" onClick={() => setSelectedCountry('germany')}>Германия</button>
         <button className="country-btn" onClick={() => setSelectedCountry('england')}>Англия</button>
+      </div>
       </div>
       <div className='items'>
         {filteredData.length > 0 ? (
           filteredData.map((item, index) => (
             <div key={index} className="catalogItem">
               <img src={item.image} alt={item.title} />
-              <p classname='standard-text'>{item.author}</p>
-              <h3>{item.title}</h3>
-              <p classname='standard-text'>{item.description}</p>
-              <p>{item.price}</p>
+              <p className='item-author'>{item.author}</p>
+              <h3 className='item-title'>{item.title}</h3>
+              <p className='item-description'>{item.description}</p>
+              <p className='item-price'>{item.price}</p>
               <a href="#" className="btn">В корзину</a>
             </div>
           ))
         ) : (
           <p>Нет данных для выбранной страны.</p>
         )}
-      </div>
+    </div>
     </div>
     </div>
   );
--- src/mocks/CatalogItems.js
+++ src/mocks/CatalogItems.js
@@ -1,6 +1,21 @@
 import france1 from '../img/france/france1.jpg';
 import france2 from '../img/france/france2.jpg';
 import france3 from '../img/france/france3.jpg';
+import france4 from '../img/france/france4.jpg';
+import france5 from '../img/france/france5.jpg';
+import france6 from '../img/france/france6.jpg';
+import germany1 from '../img/germany/germany1.jpg';
+import germany2 from '../img/germany/germany2.jpg';
+import germany3 from '../img/germany/germany3.jpg';
+import germany4 from '../img/germany/germany4.jpg';
+import germany5 from '../img/germany/germany5.jpg';
+import germany6 from '../img/germany/germany6.jpg';
+import england1 from '../img/england/england1.jpg';
+import england2 from '../img/england/england2.jpg';
+import england3 from '../img/england/england3.jpg';
+import england4 from '../img/england/england4.jpg';
+import england5 from '../img/england/england5.jpg';
+import england6 from '../img/england/england6.jpg';
 
 const catalogData = [
     {
@@ -28,7 +43,7 @@ const catalogData = [
         country: 'france'
     },
     {
-        image: '/img/france/france4.jpg',
+        image: france4,
         author: 'Луи Детуш',
         title: 'Роза',
         description: 'Бумага, акрил (50х80)',
@@ -36,7 +51,7 @@ const catalogData = [
         country: 'france'
     },
     {
-        image: '/img/france/france5.jpg',
+        image: france5,
         author: 'Франсуа Дюпон',
         title: 'Птичья трапеза',
         description: 'Цветная литография (40х60)',
@@ -44,7 +59,7 @@ const catalogData = [
         country: 'france'
     },
     {
-        image: '/img/france/france6.jpg',
+        image: france6,
         author: 'Пьер Моранж',
         title: 'Пейзаж с рыбой',
         description: 'Цветная литография (40х60)',
@@ -52,99 +67,99 @@ const catalogData = [
         country: 'france'
     },
     {
-        image: '/img/france/france1.jpg',
-        author: 'lol',
-        title: 'Охота Амура',
+        image: germany1,
+        author: 'Курт Вернер',
+        title: 'Над городом',
+        description: 'Цветная литография (40х60)',
+        price: '16 000 руб',
+        country: 'germany'
+    },
+    {
+        image: germany2,
+        author: 'Макс Рихтер',
+        title: 'Птенцы',
         description: 'Холст, масло (50х80) ',
         price: '14 500 руб',
         country: 'germany'
     },
     {
-        image: '/img/france/france2.jpg',
-        author: 'Анри Селин',
-        title: 'Дама с собачкой',
-        description: 'Акрил, бумага (50х80)',
-        price: '16 500 руб',
-        country: 'germany'
-    },
-    {
-        image: '/img/france/france3.jpg',
-        author: 'Франсуа Дюпон',
-        title: 'Процедура',
-        description: 'Цветная литография (40х60)',
+        image: germany3,
+        author: 'Мартин Майер',
+        title: 'Среди листьев',
+        description: 'Цветная литография (40х60) ',
         price: '20 000 руб',
         country: 'germany'
     },
     {
-        image: '/img/france/france4.jpg',
-        author: 'Луи Детуш',
-        title: 'Роза',
-        description: 'Бумага, акрил (50х80)',
-        price: '12 000 руб',
+        image: germany4,
+        author: 'Герман Беккер',
+        title: 'Яркая птица',
+        description: 'Цветная литография (40х60) ',
+        price: '13 000 руб',
         country: 'germany'
     },
     {
-        image: '/img/france/france5.jpg',
-        author: 'Франсуа Дюпон',
-        title: 'Птичья трапеза',
-        description: 'Цветная литография (40х60)',
-        price: '22 500 руб',
-        country: 'germany'
-    },
-    {
-        image: '/img/france/france6.jpg',
-        author: 'Пьер Моранж',
-        title: 'Пейзаж с рыбой',
-        description: 'Цветная литография (40х60)',
+        image: germany5,
+        author: 'Вульф Бауэр',
+        title: 'Дятлы',
+        description: 'Бумага, акрил (50х80) ',
         price: '20 000 руб',
         country: 'germany'
     },
     {
-        image: '/img/france/france1.jpg',
-        author: 'kek',
-        title: 'Охота Амура',
-        description: 'Холст, масло (50х80) ',
-        price: '14 500 руб',
+        image: germany6,
+        author: 'Вальтер Хартманн',
+        title: 'Большие воды',
+        description: 'Бумага, акрил (50х80) ',
+        price: '23 000 руб',
+        country: 'germany'
+    },
+    {
+        image: england1,
+        author: 'Пол Смит',
+        title: 'Дикий зверь',
+        description: 'Акварель, бумага (50х80) ',
+        price: '19 500 руб',
         country: 'england'
     },
     {
-        image: '/img/france/france2.jpg',
-        author: 'Анри Селин',
-        title: 'Дама с собачкой',
-        description: 'Акрил, бумага (50х80)',
-        price: '16 500 руб',
+        image: england2,
+        author: 'Джон Уайт',
+        title: 'Скалистый берег',
+        description: 'Цветная литография (40х60) ',
+        price: '17 500 руб',
         country: 'england'
     },
     {
-        image: '/img/france/france3.jpg',
-        author: 'Франсуа Дюпон',
-        title: 'Процедура',
-        description: 'Цветная литография (40х60)',
-        price: '20 000 руб',
+        image: england3,
+        author: 'Джим Уотсон',
+        title: 'Река и горы',
+        description: 'Акварель, бумага (50х80) ',
+        price: '20 500 руб',
         country: 'england'
     },
     {
-        image: '/img/france/france4.jpg',
-        author: 'Луи Детуш',
-        title: 'Роза',
-        description: 'Бумага, акрил (50х80)',
-        price: '12 000 руб',
+        image: england4,
+        author: 'Юджин Зиллион',
+        title: 'Белый попугай',
+        description: 'Цветная литография (40х60) ',
+        price: '15 500 руб',
         country: 'england'
     },
     {
-        image: '/img/france/france5.jpg',
-        author: 'Франсуа Дюпон',
-        title: 'Птичья трапеза',
-        description: 'Цветная литография (40х60)',
-        price: '22 500 руб',
+        image: england5,
+        author: 'Эрик Гиллман',
+        title: 'Ночная рыба',
+        description: 'Бумага, акрил (50х80) ',
+        price: '12 500 руб',
         country: 'england'
     },
     {
-        image: '/img/france/france6.jpg',
-        author: 'Пьер Моранж',
-        title: 'Пейзаж с рыбой',
-        description: 'Цветная литография (40х60)',
-        price: '20 000 руб',
+        image: england6,
+        author: 'Альфред Барр',
+        title: 'Рыжий кот',
+        description: 'Цветная литография (40х60) ',
+        price: '21 000 руб',
         country: 'england'
     }
 ]
