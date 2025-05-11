d8c74d16ab90347cf5c5cbbe0adf90d576dc31d0
anastynero <anastasia1995ggs@gmail.com>
2025-05-11 17:18:48 +0300

    add promo section

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,10 +1,23 @@
 import '../../style/App.css';
+import star from './../../img/star.svg';
 
 function Promo() {
   return (
-    <div className='promo'>
-        
-    </div>
+    <section className='promo'>
+      <div className='container'>
+        <div className='promo__content'>
+          <div className='promo__title'>
+            <img src={star} alt=""/>
+            <h2>Новая коллекция французских авторов</h2>
+          </div>
+          <p>Сложно сказать, почему акционеры крупнейших компаний призывают нас к&nbsp;новым свершениям, 
+            которые, в&nbsp;свою очередь, должны быть заблокированы в&nbsp;рамках своих собственных рациональных ограничений. 
+        Принимая во&nbsp;внимание показатели успешности, граница обучения кадров предопределяет высокую востребованность 
+        направлений прогрессивного развития.</p>
+        <button className="btn-yellow">Ознакомиться</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
